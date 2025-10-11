120a1b776695fc1151ec5885378caec099f5c248
João Ramajo <joaoramajo744@gmail.com>
2025-10-11 14:27:15 -0300

    test: adiciona testes para FileReader.php cobrindo casos de arquivo encontrado e arquivo inexistente

---
+++ tests/Unit/Infra/Readers/FileReader/ReadFilesTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Ramajo\Infra\Readers\FileReader;
+
+test('retorna um array com as informações de um arquivo corretamente', function() {
+    $file = dirname(__DIR__, 5) . '/mock/arquivo.log';
+
+    $reader = new FileReader();
+
+    $res = $reader->read($file);
+
+    expect($res)->toBeArray();
+});
+
+test('lança uma excessão se não encontrar o arquivo', function() {
+    $file = 'arquivo-inexistente.log';
+
+    $reader = new FileReader();
+
+    $reader->read($file);
+})->throws(\Exception::class);
