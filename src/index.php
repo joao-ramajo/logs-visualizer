9b00de5e20adc0db85b9baf02529f04f92b6f3f2
João Ramajo <joaoramajo744@gmail.com>
2025-10-12 01:42:26 -0300

    feat: adiciona LogVisualizer como orquestrador atraves de uma strategy para manipular uma fonte de informações

--- src/index.php
+++ src/index.php
@@ -2,23 +2,27 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
+use Ramajo\App\LogVisualizer;
 use Ramajo\Core\Entities\File;
 use Ramajo\Infra\Adapters\MonologAdapter;
 use Ramajo\Infra\Readers\FileReader;
+use Ramajo\Infra\Strategies\MonologStrategy;
 
+// $reader = new FileReader();
 
-$reader = new FileReader();
+// $file = new File('mock/arquivo.log');
 
-$file = new File('mock/arquivo.log');
+// $res = $reader->read($file);
 
-$res = $reader->read($file);
+// $adapter = new MonologAdapter();
 
-$adapter = new MonologAdapter();
+// $collection = $adapter->parse($res);
 
-$collection = $adapter->parse($res);
-
-var_dump($collection);
+// var_dump($collection);
 // $tail = $reader->tail('mock/arquivo.log');
 
 // $collection = $adapter->parse($tail);
 
+$visualizer = new LogVisualizer('mock/arquivo.log', new MonologStrategy());
+
+var_dump($visualizer->tail());
