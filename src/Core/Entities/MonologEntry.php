9dc3074c3ece14af7e491e25186ba33626f837e4
João Ramajo <joaoramajo744@gmail.com>
2025-10-12 15:17:49 -0300

    doc: adiciona PHPDoc na classe MonologEntry

--- src/Core/Entities/MonologEntry.php
+++ src/Core/Entities/MonologEntry.php
@@ -5,8 +5,20 @@ namespace Ramajo\Core\Entities;
 use Ramajo\Core\Interfaces\LogEntryInterface;
 use DateTimeImmutable;
 
+/**
+ * Entidade que representa uma informação(linha) de um arquivo de logs
+ */
 class MonologEntry implements LogEntryInterface
 {
+    /**
+     * MonologEntry constructor.
+     *
+     * @param DateTimeImmutable $timestamp Data e hora do log
+     * @param string $channel Canal do log
+     * @param string $level Nível do log
+     * @param string $message Conteúdo principal do log
+     * @param string|null $context Contexto do log ou stack traces
+     */
     public function __construct(
         public DateTimeImmutable $timestamp,
         public string $channel,
@@ -15,31 +27,61 @@ class MonologEntry implements LogEntryInterface
         public ?string $context = null
     ) {}
 
+    /**
+     * Retorna a data e hora do log
+     *
+     * @return DateTimeImmutable
+     */
     public function getTimestamp(): DateTimeImmutable
     {
         return $this->timestamp;
     }
 
+    /**
+     * Retorna o nível do LOG
+     *
+     * @return string
+     */
     public function getLevel(): string
     {
         return $this->level;
     }
 
+    /**
+     * Retorna o canal do LOG
+     *
+     * @return string
+     */
     public function getChannel(): string
     {
         return $this->channel;
     }
 
+    /**
+     * Retorna o conteúdo do LOG
+     *
+     * @return string
+     */
     public function getMessage(): string
     {
         return $this->message;
     }
 
+    /**
+     * Retorna o contexto do log ou null se não houver.
+     *
+     * @return string|null
+     */
     public function getContext(): ?string
     {
         return $this->context;
     }
 
+    /**
+     * Retorna o conteúdo do log como array associativo.
+     *
+     * @return array<string, mixed> Cada chave representa uma propriedade do log
+     */
     public function toArray(): array
     {
         return [
@@ -51,6 +93,11 @@ class MonologEntry implements LogEntryInterface
         ];
     }
 
+    /**
+     * Retorna o conteúdo do log como string JSON.
+     *
+     * @return string JSON representando a entidade de log
+     */
     public function toJson(): string
     {
         return json_encode($this->toArray());
