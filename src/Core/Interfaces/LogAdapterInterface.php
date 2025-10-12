a3ecebb3d2528663cf1e410b233c6f6baa0a590d
João Ramajo <joaoramajo744@gmail.com>
2025-10-12 15:30:53 -0300

    doc: adiciona documentação por PHPDoc para a interface LogAdapterInterface

--- src/Core/Interfaces/LogAdapterInterface.php
+++ src/Core/Interfaces/LogAdapterInterface.php
@@ -2,9 +2,28 @@
 
 namespace Ramajo\Core\Interfaces;
 
+/**
+ * Interace para os Adapters
+ *
+ * Define os métodos básicos que todo adapter deve conter para processar as informações dos arquivos.
+ */
 interface LogAdapterInterface
 {
+    /**
+     * Realiza o parse inicial de um conjunto de linhas de log.
+     *
+     * @param string[] $lines Array de linhas de um arquivo de log.
+     *
+     * @return EntryCollectionInterface<LogEntryInterface> Coleção de entradas de log estruturadas.
+     */
     public function parse(array $lines): EntryCollectionInterface;
 
+    /**
+     * Converte uma coleção de entradas de log em JSON.
+     *
+     * @param EntryCollectionInterface<LogEntryInterface> $collection Coleção de entradas de log.
+     *
+     * @return string JSON representando todas as entradas da coleção.
+     */
     public function toJson(EntryCollectionInterface $collection): string;
 }
